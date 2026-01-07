0f8029ab2ae2a095517f1692b7a09b33b430662b
Christoph Gohlke <cgohlke@cgohlke.com>
2026-01-06 18:35:46 -0800

    Update chebyfit/conftest.py

--- chebyfit/conftest.py
+++ chebyfit/conftest.py
@@ -7,7 +7,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def doctest_config(doctest_namespace):
+def doctest_config(doctest_namespace) -> None:
     """Add numpy to doctest namespace."""
     numpy.set_printoptions(suppress=True, precision=2)
     doctest_namespace['numpy'] = numpy
